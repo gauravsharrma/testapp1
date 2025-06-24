575a251cbd21da5489b14a48740d9b0e65833f65
gauravsharrma <gauravsharrma00@gmail.com>
2025-06-24 22:58:51 +0400

    Add simple homepage form and greeting route

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello, world!' });
+});
+
+app.post('/submit', (req, res) => {
+  const name = req.body.name || 'World';
+  res.send(`<h1>Hello, ${name}!</h1>`);
 });
 
 app.listen(port, () => {
